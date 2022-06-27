6ee80d4a3457e97c56d67e753eafe3002da7634c
domainhaapka <106553139+domainhaapka@users.noreply.github.com>
2022-06-27 08:50:58 +0530

    Update uploader.py

--- bot/helpers/uploader.py
+++ bot/helpers/uploader.py
@@ -69,7 +69,7 @@ class Uploader:
             )
             description = (
                 Config.VIDEO_DESCRIPTION
-                + "\nUploaded to YouTube with https://tx.me/youtubeitbot"
+                + "\n~~~~enjoy"
             )[:5000]
             if not Config.UPLOAD_MODE:
                 privacyStatus = "private"
